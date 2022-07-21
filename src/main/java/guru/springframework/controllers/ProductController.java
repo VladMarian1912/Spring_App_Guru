2cbf27820699dd74c3f6e7c2a3f61ad4fceab72a
vladut.marinescu <vladmarinescu1912@yahoo.com>
2022-07-21 12:14:37 +0300

    put @RequestMapping("/product") on ProductController class

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
     private ProductService productService;
@@ -20,37 +21,37 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/product/list")
+    @RequestMapping({"/list", "/"})
     public String listProducts(Model model){
         model.addAttribute("products", productService.listAll());
         return "product/list";
     }
 
-    @RequestMapping("/product/show/{id}")
+    @RequestMapping("/show/{id}")
     public String getProduct(@PathVariable Integer id, Model model){
         model.addAttribute("product", productService.getById(id));
         return "product/show";
     }
 
-    @RequestMapping("product/edit/{id}")
+    @RequestMapping("/edit/{id}")
     public String edit(@PathVariable Integer id, Model model){
         model.addAttribute("product", productService.getById(id));
         return "product/productform";
     }
 
-    @RequestMapping("/product/new")
+    @RequestMapping("/new")
     public String newProduct(Model model){
         model.addAttribute("product", new Product());
         return "product/productform";
     }
 
-    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public String saveOrUpdateProduct(Product product){
         Product savedProduct = productService.saveOrUpdate(product);
         return "redirect:/product/show/" + savedProduct.getId();
     }
 
-    @RequestMapping("/product/delete/{id}")
+    @RequestMapping("/delete/{id}")
     public String delete(@PathVariable Integer id){
         productService.delete(id);
         return "redirect:/product/list";
