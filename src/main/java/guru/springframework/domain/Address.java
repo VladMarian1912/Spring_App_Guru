9c2dfb47ba0032193b4e480e2d1a89ffa50751f9
vladut.marinescu <vladmarinescu1912@yahoo.com>
2022-07-26 09:38:46 +0300

    put @ToString in Address class

--- src/main/java/guru/springframework/domain/Address.java
+++ src/main/java/guru/springframework/domain/Address.java
@@ -2,12 +2,14 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
 @Setter
+@ToString()
 public class Address {
 
     private String addressLine1;
